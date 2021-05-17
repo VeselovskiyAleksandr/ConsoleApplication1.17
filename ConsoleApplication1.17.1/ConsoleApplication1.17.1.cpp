b3fd0067ead21747a5d42a99a34f116d4d898b1a
Alexandr <VeselovskiyAlexandr@mail.ru>
2021-05-17 10:41:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.17.1/ConsoleApplication1.17.1.cpp
@@ -0,0 +1,120 @@
+﻿// ConsoleApplication1.17.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Урок 14. Задача 2.
+
+#include <iostream>
+#include <locale.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char tic_tac_toe[3][3] = { {' ',' ',' '},
+							 {' ',' ',' '},
+							 {' ',' ',' '} };
+
+	bool correct = true;
+	int i, j, countX = 0, countO = 0, countWinXI = 0, countWinOI=0, countWinXIJ = 0, countWinOIJ = 0;
+	int countWinXJ = 0, countWinOJ = 0, countWinXJI = 0, countWinOJI = 0;
+		
+	do {
+cout << "\n Игровое поле: \n";
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				cout << tic_tac_toe[i][j]<<" ";
+			}
+			cout << "\n";
+		}
+		if ((countWinXI == 3)||(countWinXJ == 3)||(countWinXIJ == 3)||(countWinXJI == 3)) {
+			cout << "\n Победили крестики!";
+			break;
+		}
+		else if ((countWinOI == 3)||(countWinOJ == 3)||(countWinOIJ == 3)||(countWinOJI == 3)) {
+			cout << "\n Победили нолики!";
+			break;
+		}
+		cout << "\n Укажите номер строки и столбца, выбранной клетки \n";
+		cin >> i >> j;
+		if((tic_tac_toe[i][j] == 'X')||(tic_tac_toe[i][j] =='O')) {
+			cout << "\n Данная клетка занята. Укажите правильно клетку."; 
+		}
+		else if ((i < 0) || (i > 2) || (j < 0) || (j > 2)) {
+			cout << "\n Сделайте правильный ход ";		
+		}
+		else {
+			cout << "\n Сделайте Ваш ход: ";
+			cin >> tic_tac_toe[i][j];
+			if ((tic_tac_toe[i][j] != 'X') && (tic_tac_toe[i][j] != 'O')) {
+				cout << "\n Сделайте правильный ход ";
+				tic_tac_toe[i][j] == ' ';	
+			}
+			else {
+				if (tic_tac_toe[i][j] == 'X') {
+					countX++; countO = 0;
+					if (countX > 1) {
+						cout << "\n Сделайте правильный ход ";
+						tic_tac_toe[i][j] = ' ';
+					}
+				}
+				else if (tic_tac_toe[i][j] == 'O') {
+					countO++; countX = 0;
+					if (countO > 1) {
+						cout << "\n Сделайте правильный ход ";
+						tic_tac_toe[i][j] = ' ';
+					}
+				}
+			 }
+		  }	
+		if (correct) {
+			countWinXIJ = 0; countWinOIJ = 0; countWinXJI = 0; countWinOJI = 0;
+			countWinXI = 0; countWinOI = 0;  countWinXJ = 0; countWinOJ = 0;
+			for (i = 0; i < 3; i++) {
+				for (j = 0; j < 3; j++) {
+					if (tic_tac_toe[i][j] == 'X') {
+						countWinXJ++;	
+						if (countWinXJ == 3) {
+							break;
+						}
+					}
+					else if (tic_tac_toe[i][j] == 'O') {
+						countWinOJ++;
+						if (countWinOJ == 3) {
+							break;
+						}
+					}
+				if ((i == j) && (tic_tac_toe[i][j] == 'X')) {
+					countWinXIJ++; 
+                 }
+				else if ((i == j) && (tic_tac_toe[i][j] == 'O')) {
+					countWinOIJ++;
+				}
+				 if ((i == 2 - j) && (tic_tac_toe[i][j] == 'X')) {
+					
+					countWinXJI++;
+				}
+				else if ((j ==2-i) && (tic_tac_toe[i][j] == 'O')) {
+					countWinOJI++;
+				}
+	if (tic_tac_toe[i][j] == 'X') {
+					countWinXI++;
+				}
+				else if(tic_tac_toe[i][j] == 'O') {
+					countWinOI++;
+			    	}
+		    	} 		
+			 }
+		  }
+		} while (true);
+	}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
