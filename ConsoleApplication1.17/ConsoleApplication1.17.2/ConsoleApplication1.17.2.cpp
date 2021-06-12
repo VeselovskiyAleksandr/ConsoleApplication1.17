dc5669dd7d1ac2f274fdf72a3d15eab329caf94b
Alexandr <VeselovskiyAlexandr@mail.ru>
2021-06-12 13:06:49 +0300

    test 1

--- ConsoleApplication1.17/ConsoleApplication1.17.2/ConsoleApplication1.17.2.cpp
+++ ConsoleApplication1.17/ConsoleApplication1.17.2/ConsoleApplication1.17.2.cpp
@@ -3,15 +3,51 @@
 
 #include <iostream>
 #include <locale.h>
-#include <vector>
 using namespace std;
+
+void inicialization(bool array[][12], int t) {
+for (int i = 0; i < t; i++) {
+		for (int j = 0; j < 12; j++) {
+			if ((i ==t-1) && (j != 0 && j != 6)) {
+				array[i][j] = false;
+			}
+			else {
+				array[i][j] = true;
+			}	
+		}
+	}
+}
+
+void outputArray(bool array[][12], int t) {
+	for (int i = 0; i < t; i++) {
+			for (int j = 0; j < 12; j++) {
+				cout << (int) array[i][j];
+	      }
+			cout << "\n";
+    }
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	vector <vector<int>> banquetHoll{
-	};
-	<int> vrctor silverware[2][6];
+	bool silverware[4][12], dinnerWare[3][12];
+	int chairWare[2][6];
+	inicialization(silverware, 4);
+	inicialization(dinnerWare,3);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 6; j++) {
+			chairWare[i][j] = 1;
+		}
+	}
+	chairWare[0][4] = 2;
+	silverware[0][8] = false;
+	silverware[0][6] = false;
+	silverware[3][8] = true; 
+	dinnerWare[2][6] = false;
+	cout << "\n Наличие столовых приборов к концу обеда\n";
+	outputArray(silverware, 4);
+	cout << "\n Наличие посуды к концу обеда\n";
+	outputArray(dinnerWare, 3);
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
